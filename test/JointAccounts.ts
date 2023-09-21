f987581cce7b2ab2ecd26a93dcf2bc5a3ca9ed6d
jay-web <jay.developer12@gmail.com>
2023-09-21 18:48:35 +0530

    added 22 test cases for all functionality and there edge cases in testing script of the smart contract

--- test/JointAccounts.ts
+++ test/JointAccounts.ts
@@ -12,13 +12,38 @@ describe("JointAccounts", function () {
   // and reset Hardhat Network to that snapshot in every test.
   async function deployJointAccount() {
     // Contracts are deployed using the first signer/account by default
-    const [ownerAddress, addr1, addr2, addr3, addr4] =
+    const [addr, addr1, addr2, addr3, addr4] =
       await ethers.getSigners();
 
     const JointAccount = await ethers.getContractFactory("JointAccounts");
     const jointAccount = await JointAccount.deploy();
 
-    return { jointAccount, ownerAddress, addr1, addr2, addr3, addr4 };
+    return { jointAccount, addr, addr1, addr2, addr3, addr4 };
+  }
+
+
+  async function deployJointAccountWithAccounts(owners = 1, deposit = 0, withdrawAmounts:any[] = []){
+    const { jointAccount, addr, addr1,addr2, addr3, addr4} = await loadFixture(deployJointAccount);
+    // ! Note - addr is the creator of the account , depositor and withdraw requestor here
+    // ! This is pre defined account for testing purpose
+    
+    let addresses:string[] = [];
+
+    if(owners == 2) { addresses = [addr1.address]}
+    else if(owners == 3) { addresses = [addr1.address, addr2.address]}
+    else if(owners == 4) { addresses = [ addr1.address,addr2.address, addr3.address]}
+
+    await jointAccount.connect(addr).createAccount(addresses);
+
+    if(deposit > 0){
+      await jointAccount.connect(addr).deposit(0, { value: deposit.toString()})
+    }
+
+    for(let amount of withdrawAmounts){
+      await jointAccount.connect(addr).requestOfWithdrawal(0, amount)
+    }
+
+    return { jointAccount, addr, addr1, addr2, addr3, addr4}
   }
 
   describe("Deployment", function () {
@@ -29,21 +54,21 @@ describe("JointAccounts", function () {
 
   describe("Creating an account", () => {
     it("Should allow creating a single user account", async () => {
-      const { jointAccount, ownerAddress } = await loadFixture(
+      const { jointAccount, addr } = await loadFixture(
         deployJointAccount
       );
-      await jointAccount.connect(ownerAddress).createAccount([]);
-      const accounts = await jointAccount.connect(ownerAddress).getAccounts();
+      await jointAccount.connect(addr).createAccount([]);
+      const accounts = await jointAccount.connect(addr).getAccounts();
       expect(accounts.length).to.equal(1);
     });
 
     it("Should allow to create double user account", async () => {
-      const { jointAccount, ownerAddress, addr1 } = await loadFixture(
+      const { jointAccount, addr, addr1 } = await loadFixture(
         deployJointAccount
       );
-      await jointAccount.connect(ownerAddress).createAccount([addr1.address]);
+      await jointAccount.connect(addr).createAccount([addr1.address]);
 
-      const account1 = await jointAccount.connect(ownerAddress).getAccounts();
+      const account1 = await jointAccount.connect(addr).getAccounts();
       expect(account1.length).to.equal(1);
 
       const account2 = await jointAccount.connect(addr1).getAccounts();
@@ -51,14 +76,14 @@ describe("JointAccounts", function () {
     });
 
     it("Should allow to create triple user account", async () => {
-      const { jointAccount, ownerAddress, addr1, addr2 } = await loadFixture(
+      const { jointAccount, addr, addr1, addr2 } = await loadFixture(
         deployJointAccount
       );
       await jointAccount
-        .connect(ownerAddress)
+        .connect(addr)
         .createAccount([addr1.address, addr2.address]);
 
-      const account1 = await jointAccount.connect(ownerAddress).getAccounts();
+      const account1 = await jointAccount.connect(addr).getAccounts();
       expect(account1.length).to.equal(1);
 
       const account2 = await jointAccount.connect(addr1).getAccounts();
@@ -69,14 +94,14 @@ describe("JointAccounts", function () {
     });
 
     it("Should allow to create quad user account", async () => {
-      const { jointAccount, ownerAddress, addr1, addr2, addr3 } =
+      const { jointAccount, addr, addr1, addr2, addr3 } =
         await loadFixture(deployJointAccount);
 
       await jointAccount
-        .connect(ownerAddress)
+        .connect(addr)
         .createAccount([addr1.address, addr2.address, addr3.address]);
 
-      const account1 = await jointAccount.connect(ownerAddress).getAccounts();
+      const account1 = await jointAccount.connect(addr).getAccounts();
       expect(account1.length).to.equal(1);
 
       const account2 = await jointAccount.connect(addr1).getAccounts();
@@ -90,21 +115,21 @@ describe("JointAccounts", function () {
     });
 
     it("Should not allow creating a duplicate user account", async () => {
-      const { jointAccount, ownerAddress } = await loadFixture(
+      const { jointAccount, addr } = await loadFixture(
         deployJointAccount
       );
       await expect(
-        jointAccount.connect(ownerAddress).createAccount([ownerAddress.address])
+        jointAccount.connect(addr).createAccount([addr.address])
       ).to.be.reverted;
     });
 
     it("Should not allow to create account which already have 4 owners", async () => {
-      const { jointAccount, ownerAddress, addr1, addr2, addr3, addr4 } =
+      const { jointAccount, addr, addr1, addr2, addr3, addr4 } =
         await loadFixture(deployJointAccount);
       await expect(jointAccount
-        .connect(ownerAddress)
+        .connect(addr)
         .createAccount([
-          ownerAddress.address,
+          addr.address,
           addr1.address,
           addr2.address,
           addr3.address,
@@ -113,7 +138,7 @@ describe("JointAccounts", function () {
     });
 
     it("Should not allow to create account more than 3", async () => {
-      const { jointAccount, ownerAddress, addr1 } = await loadFixture(deployJointAccount);
+      const { jointAccount, addr, addr1 } = await loadFixture(deployJointAccount);
         for (let index = 0; index < 3; index++) {
           await jointAccount
                     .connect(addr1)
@@ -124,6 +149,120 @@ describe("JointAccounts", function () {
       
     });
 
-
   });
+
+  describe("Depositing", async () => {
+    
+    it("Shuuld allowed deposit from the account owner", async () => {
+      const {jointAccount, addr} = await deployJointAccountWithAccounts(1);
+      await expect(jointAccount.connect(addr).deposit(0, { value: "100"})).to.changeEtherBalances([jointAccount, addr], ["100", "-100"])
+      
+    });
+
+    it("Shuuld not allowed deposit from the non=owner account", async () => {
+      const {jointAccount ,addr1} = await deployJointAccountWithAccounts(1);
+      await expect(jointAccount.connect(addr1).deposit(0, { value: "100"})).to.be.reverted;
+    })
+   
+  });
+
+
+  // ! Test cases for withdraw
+
+  describe("Withdraw", () => {
+    
+    describe("request withdraw", () => {
+      it("Should be able to create with request by owner", async () => {
+        const { jointAccount, addr } = await deployJointAccountWithAccounts(0, 100);
+        await jointAccount.connect(addr).requestOfWithdrawal(0, 100);
+
+      });
+
+      it("Should not be able to create withdraw request with invalid amount", async () => {
+        const { jointAccount, addr } = await deployJointAccountWithAccounts(0, 100);
+
+        await expect(jointAccount.connect(addr).requestOfWithdrawal(0, 102)).to.be.reverted;
+      });
+
+      it("Should not be able to create withdraw request with invalid account", async () => {
+        const { jointAccount, addr1 } = await deployJointAccountWithAccounts(0, 100);
+
+        await expect(jointAccount.connect(addr1).requestOfWithdrawal(0, 10)).to.be.reverted;
+      });
+
+      it("Should be able to create mulitple withdraw request", async () => {
+        const { jointAccount, addr } = await deployJointAccountWithAccounts(0, 100);
+
+        await jointAccount.connect(addr).requestOfWithdrawal(0, 10);
+        await jointAccount.connect(addr).requestOfWithdrawal(0, 50);
+        await jointAccount.connect(addr).requestOfWithdrawal(0, 40);
+      })
+
+
+    });
+
+    describe("approve withdraw request", () => {
+      it("Shuuld allow account owner to approve withdraw", async () => {
+        const { jointAccount, addr1} = await deployJointAccountWithAccounts(2, 100, [100]);
+
+        await jointAccount.connect(addr1).approvalOfWithdrawal(0, 0);
+
+        expect(await jointAccount.getApprovals(0, 0)).to.equal(1);
+      });
+
+      it("Shuuld not allow non-account owner to approve withdraw", async () => {
+        const { jointAccount, addr2} = await deployJointAccountWithAccounts(2, 100, [100]);
+
+        await expect(jointAccount.connect(addr2).approvalOfWithdrawal(0, 0)).to.be.rejected;
+      });
+
+      it("Shuuld not allow  owner to approve withdraw mulitple times", async () => {
+        const { jointAccount, addr1} = await deployJointAccountWithAccounts(2, 100, [100]);
+
+        await jointAccount.connect(addr1).approvalOfWithdrawal(0, 0);
+        await expect(jointAccount.connect(addr1).approvalOfWithdrawal(0, 0)).to.be.rejected;
+      });
+
+      it("Shuuld not allow creator of withdraw request to approve withdraw", async () => {
+        const { jointAccount, addr} = await deployJointAccountWithAccounts(2, 100, [100]);
+
+        
+        await expect(jointAccount.connect(addr).approvalOfWithdrawal(0, 0)).to.be.rejected;
+      });
+
+    });
+
+    describe("make a withdraw", ()=> {
+      it("Shuuld allow creator of withdraw request to withdraw approved request", async () => {
+        const { jointAccount, addr, addr1} = await deployJointAccountWithAccounts(2, 100, [100]);
+
+        await jointAccount.connect(addr1).approvalOfWithdrawal(0, 0);
+        await expect(jointAccount.connect(addr).withdraw(0, 0)).to.changeEtherBalances([jointAccount, addr], ["-100", "+100"])
+      });
+
+      it("Shuuld not allow creator of withdraw request to withdraw twice to the approved request", async () => {
+        const { jointAccount, addr, addr1} = await deployJointAccountWithAccounts(2, 200, [100]);
+
+        await jointAccount.connect(addr1).approvalOfWithdrawal(0, 0);
+        await expect(jointAccount.connect(addr).withdraw(0, 0)).to.changeEtherBalances([jointAccount, addr], ["-100", "+100"]);
+        await expect(jointAccount.connect(addr).withdraw(0, 0)).to.be.reverted;
+      });
+
+      it("Shuuld not allow not-creator of withdraw request to withdraw the approved request", async () => {
+        const { jointAccount, addr, addr1} = await deployJointAccountWithAccounts(2, 200, [100]);
+
+        await jointAccount.connect(addr1).approvalOfWithdrawal(0, 0);
+        await expect(jointAccount.connect(addr1).withdraw(0, 0)).to.be.reverted;
+      });
+
+      it("Shuuld not allow non-approved withdraw request to withdraw ", async () => {
+        const { jointAccount, addr, addr1} = await deployJointAccountWithAccounts(2, 200, [100]);
+
+        await expect(jointAccount.connect(addr).withdraw(0, 0)).to.be.reverted;
+      });
+    });
+  })
+
+
+
 });
